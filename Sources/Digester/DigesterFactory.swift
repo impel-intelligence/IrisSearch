b16d4f0092de35d219bf4f379e19a1727b3e2a98
Taylor Lineman <git@actuallytaylor.com>
2026-06-16 19:26:10 -0400

    Made digester factory public
    Signed-off-by: Taylor Lineman <git@actuallytaylor.com>

--- Sources/Digester/DigesterFactory.swift
+++ Sources/Digester/DigesterFactory.swift
@@ -7,14 +7,14 @@
 
 import UniformTypeIdentifiers
 
-struct DigesterFactory {
+public struct DigesterFactory {
     private static let registeredDigesters: [FileDigester.Type] = [TXTDigester.self, PDFDigester.self]
     
-    static var availableUniformTypes: [UTType] {
+    public static var availableUniformTypes: [UTType] {
         return registeredDigesters.flatMap({$0.fileTypes})
     }
     
-    static func digester(for type: UTType) throws -> FileDigester {
+    public static func digester(for type: UTType) throws -> FileDigester {
         guard let digesterType = registeredDigesters.first(where: { $0.fileTypes.contains(type) }) else {
             throw DigestionError.noAvailableDigester(type: type)
         }
